cb59244fd270d5fe2f82c11f6671f86f75f39fc6
Islam Atwa <Islam Atwa>
2025-01-29 23:59:26 +0200

    Adeded Swiper Css and Js libaray | implemented Swiper for the card section

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ function changeBacgroundColorNavbar(){
     if (this.scrollY <= 150)header.classList.remove('scroll-nav');
 }
 window.addEventListener('scroll', changeBacgroundColorNavbar);
+
+// =========== SWIPER ===========// 
+
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,  // المسافة بين الشرائح
+    grabCursor: true,  // يجعل المؤشر يبدو كأنه يمسك العنصر
+    centeredSlides: true,  // تصحيح الخطأ: يجب أن يكون centeredSlides وليس centerslides
+    loop: true,  // يجعل التمرير دائريًا
+    slidesPerView: "auto",  // يحدد عدد الشرائح المرئية تلقائيًا وفقًا للحجم
+
+    navigation: {
+        nextEl: ".swiper-button-next",  // زر التمرير للأمام
+        prevEl: ".swiper-button-prev",  // زر التمرير للخلف
+    },
+});
