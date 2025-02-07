ee0acd56e022306b7343ec6ad4db410fe90ed5f8
Islam Atwa <Islam Atwa>
2025-02-08 01:25:55 +0200

    create responsive.css file and create responseve for large screen

--- main.js
+++ main.js
@@ -11,16 +11,28 @@ window.addEventListener('scroll', changeBacgroundColorNavbar);
 // =========== SWIPER ===========// 
 
 var swiperPopular = new Swiper(".popular__container", {
-    spaceBetween: 32,  // المسافة بين الشرائح
+    spaceBetween: 20,  // المسافة بين الشرائح
     grabCursor: true,  // يجعل المؤشر يبدو كأنه يمسك العنصر
     centeredSlides: true,  // تصحيح الخطأ: يجب أن يكون centeredSlides وليس centerslides
     loop: true,  // يجعل التمرير دائريًا
-    slidesPerView: "auto",  // يحدد عدد الشرائح المرئية تلقائيًا وفقًا للحجم
+    slidesPerView: 3,  // يحدد عدد الشرائح المرئية تلقائيًا وفقًا للحجم
 
     navigation: {
         nextEl: ".swiper-button-next",  // زر التمرير للأمام
         prevEl: ".swiper-button-prev",  // زر التمرير للخلف
     },
+
+    breakpoints: {
+        250: {
+            slidesPerView: 1, // على الشاشات الصغيرة (الموبايل) يظهر 1 فقط
+        },
+        768: {
+            slidesPerView: 2, // على التابلت يظهر 2
+        },
+        1024: {
+            slidesPerView: 3, // على الديسكتوب يظهر 3
+        }
+    }
 });
 
 // =========== Value===============
